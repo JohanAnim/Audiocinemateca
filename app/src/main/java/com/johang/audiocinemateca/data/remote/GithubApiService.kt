33d73803ec253991b9fcda4ed8242ddf3d451c4b
JohanAnim <gutierezjohanantonio@gmail.com>
2025-09-18 18:46:52 -0500

    fix(update): Corregir URL para la comprobación de actualizaciones
    Se actualiza la URL en GithubApiService para apuntar al repositorio correcto ('Audiocinemateca' en lugar de 'Audiocinemateca_android_releases').

--- app/src/main/java/com/johang/audiocinemateca/data/remote/GithubApiService.kt
+++ app/src/main/java/com/johang/audiocinemateca/data/remote/GithubApiService.kt
@@ -5,6 +5,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface GithubApiService {
-    @GET("repos/JohanAnim/Audiocinemateca_android_releases/releases/latest")
+    @GET("repos/JohanAnim/Audiocinemateca/releases/latest")
     suspend fun getLatestRelease(): Response<GithubRelease>
 }
